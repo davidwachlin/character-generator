400d1f636d2830b99c73c28e303be0aa415b718d
David Wachlin <davidwachlin@gmail.com>
2020-07-12 15:58:24 -0400

    * CharacterCommand.csproj: adds charactercommand project
    * Program.cs:

---
+++ CharacterCommand/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CharacterLibrary;
+
+namespace CharacterCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
